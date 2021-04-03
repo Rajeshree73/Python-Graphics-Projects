935b35019fbc39e1f8a74e0317a5b00a9aa324af
rajeshree73 <rajeshreedeotaluvnit@gmail.com>
2021-04-04 00:25:20 +0530

    updated background and raindrops colour

--- raining_simulation/setup.py
+++ raining_simulation/setup.py
@@ -7,14 +7,14 @@ pg.init()
 # Screen Specification parameters
 SCREENSIZE = (720, 640)
 CAPTIONS = "Raindrops"
-BACKGROUND = (0, 30, 0)
+BACKGROUND = (135, 206, 235)
 BACKALPHA = 230
 
 # Color palette
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
-BLUE = (0, 0, 255)
+BLUE = (0, 100, 255)
 
 DISPSURFACE = pg.display.set_mode(SCREENSIZE, pg.SRCALPHA, 32)
 pg.display.set_caption(CAPTIONS)
